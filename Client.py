b52f39353a946b932659edc2c9c9ba6e78c25cc8
Redriver <80642468+TheRealRedRiver559@users.noreply.github.com>
2021-10-20 23:28:43 -0700

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,79 +1,55 @@
 import socket
 import threading
-import struct
-import stdiomask
 import datetime
 from rich import print
-
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('localhost', 9090))
 
 username = input('Username : ').lower()
-password = stdiomask.getpass(prompt='Password : ', mask='*') #Visual Password hiding.
-
-#gets time for the message. Atm its just the hard dates
-def time():
+password = input('Password : ')
+#gets time for the message.
+def get_time():
     time = datetime.datetime.now()
     setting = 'AM'
     hours = int(time.strftime("%H"))
     if hours > 12:
         setting = 'PM'
         hours -= 12
-    return time.strftime(f"%d/%m/%Y {hours}:%M:%S {setting}")
-
-def send_one_message(sock, data):
-    length = len(data)
-    sock.sendall(struct.pack('!I', length))
-    sock.sendall(data)
-
-def recv_one_message(sock):
-    lengthbuf = recvall(sock, 4)
-    length, = struct.unpack('!I', lengthbuf)
-    return recvall(sock, length)
-
-def recvall(sock, count):
-    buf = b''
-    while count:
-        newbuf = sock.recv(count)
-        if not newbuf: 
-            pass
-        buf += newbuf
-        count -= len(newbuf)
-    return buf
+    return time.strftime(f"%m/%d/%Y {hours}:%M {setting}")
 
 def client_receive():
     while True:
         try:
-            message = recv_one_message(client).decode()
+            message = client.recv(1024).decode()
             if message == 'LOGIN':
-                send_one_message(client, username.encode())
-                send_one_message(client, password.encode())
-                print(recv_one_message(client).decode())
-
-            elif message == 'CLOSE':
+                client.send(f'I{username}'.encode())
+                client.send(f'I{password}'.encode())
+                print(client.recv(1024).decode())
+            elif len(message) == 0:
                 client.close()
-                print('Connection ended...')
                 break
             elif message.startswith(username):
-                print(f"[blue]{message}[/blue] [grey37]{time()}")
+                print(f"[blue]{message[:message.find(':')]}[white]{message[message.find(':'):]}: [grey37]{get_time()}")
             elif message.startswith('Server:'):
                 print(f"[yellow]{message}")
             else:
-                print(f"[red]{message}[/red]: [grey37]{time()}")
+                print(f"[red]{message[:message.find(':')]}[white]{message[message.find(':'):]}: [grey37]{get_time()}")
         except ConnectionAbortedError:
-            print('Connection Lost...')
+            print('Connection Ended...')
             client.close()
             break
         except ConnectionResetError:
-            print('Server offline...')
+            print('Server Offline...')
             client.close()
             break
+
         
 def client_send():
     while True:
-        message = (input(""))
-        send_one_message(client, message.encode())
+        message = input("")
+        #print(f"\033[A{' '*len(message)}\033[A") currently testing. Seems to only work in the IDE
+        client.send(message.encode())
 
 receive_thread = threading.Thread(target=client_receive)
 receive_thread.start()
