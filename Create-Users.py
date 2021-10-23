55c369fb0c089bdd65060d2bfc4771df74062b38
Redriver <80642468+TheRealRedRiver559@users.noreply.github.com>
2021-10-22 21:16:58 -0700

    Update Create-Users.py

--- Create-Users.py
+++ Create-Users.py
@@ -2,9 +2,11 @@ import json
 import os.path
 from json import JSONDecodeError
 
+file_path = 'Tcp Server\\users.json'
+
 def create_user():
-    if not os.path.exists("Tcp Server\\users.json"):
-        with open("Tcp Server\\users.json", "w"):
+    if not os.path.exists(file_path):
+        with open(file_path, "w"):
             pass
     username = str(input('Username : ')).lower()
     password = str(input('Password : '))
@@ -15,18 +17,17 @@ def create_user():
             return
         elif access_level > 5:
             access_level = 5
-            print(access_level)
     except ValueError:
-        print('Only integers allowed...')
+        print('Only numbers allowed...')
         return
 
-    with open('Tcp Server\\users.json', 'r') as f:
+    with open(file_path, 'r') as f:
         try:
             data = json.load(f)
             data[username] = {"role_id": access_level, "password": password}
         except JSONDecodeError:
             data = {username:{"role_id": access_level, "password": password}}
-    with open('Tcp Server\\users.json', 'w') as f:
+    with open(file_path, 'w') as f:
         json.dump(data, f)
         print('User created')
 
