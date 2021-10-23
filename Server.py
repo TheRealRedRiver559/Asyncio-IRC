26c987397d5f141b86cbf76dcf8992acfa77d55f
Redriver <80642468+TheRealRedRiver559@users.noreply.github.com>
2021-10-22 21:16:09 -0700

    Update Server.py

--- Server.py
+++ Server.py
@@ -2,6 +2,8 @@ import socket
 import threading
 import json
 
+file_path = 'Tcp Server\\users.json' #Change path to your likings
+
 def main():
     host = 'localhost'
     port = 9090
@@ -46,24 +48,24 @@ def main():
     @command('connections', 1)
     def client_connections(username):
         client_connections = len(clients)
-        broadcast(f'{username}: {prefix}connections'.encode())
-        broadcast(f'Server: Clients online: {client_connections}'.encode())
+        broadcast(f'{username}: {prefix}connections')
+        broadcast(f'Server: Clients online: {client_connections}')
         print(f'Command: {prefix}connections (Username: {username})')
     
     #broadcasts a list of users online
     @command('users', 2)
     def users_online(username):
-        broadcast(f'{username}: {prefix}users'.encode())
+        broadcast(f'{username}: {prefix}users')
         user_list = [user for user in users]
-        broadcast(f'Server: Users: {user_list}'.encode())
+        broadcast(f'Server: Users: {user_list}')
         print(f'Server: Command: {prefix}users (Username: {username})')
     
     #sends a list of all users inside the 'banned user' list
     @command('banned-users', 3)
     def users_banned(username):
-        broadcast(f'{username}: {prefix}banned-users'.encode())
+        broadcast(f'{username}: {prefix}banned-users')
         banned_list = [user for user in banned_users]
-        broadcast(f'Server: Banned Users: {banned_list}'.encode())
+        broadcast(f'Server: Banned Users: {banned_list}')
         print(f'Server: Command: {prefix}banned-users (Username: {username})')
     
     #bans users (Command syntax : ban <username> [silent] [reason])
@@ -79,7 +81,7 @@ def main():
                 silent_ban = True
                 reason = " ".join(arguments[2:])
 
-        with open('Tcp Server\\users.json', 'r') as f:
+        with open(file_path, 'r') as f:
             data = json.load(f)
 
             user_role_id = data[username]['role_id']
@@ -87,32 +89,32 @@ def main():
             if target_username in data:
                 target_role_id = data[arguments[0]]['role_id'] 
             else:
-                client.send(f"Server: No user named '{target_username}' was found!".encode())
+                send_message(client ,f"Server: No user named '{target_username}' was found!")
                 return
             
         target_role_id = data[target_username]['role_id']
 
         if user_role_id <= target_role_id:
-            client.send(f'Server: Server: You cannot ban this member!'.encode())
+            send_message(client ,f'Server: Server: You cannot ban this member!')
             return
         if target_username in banned_users:
-            client.send(f"Server: '{target_username}' already banned!".encode())
+            send_message(client ,f"Server: '{target_username}' already banned!")
             return
         if target_username not in users:
-            client.send(f'Server: This user is not currently in the server'.encode())
+            send_message(client ,f'Server: This user is not currently in the server')
             return
 
         banned_users.append(target_username)
         target_client = clients[users.index(target_username)]
-        target_client.send(f'Server: You have been banned.\n\tReason: {reason}'.encode())
+        send_message(target_client ,f'Server: You have been banned.\n\tReason: {reason}')
         print(f'Command: {prefix}ban {target_username} Silent={silent_ban} Reason:{reason} (Username: {username})')
-        user_leave(target_client, error_message['Close'])
 
         if silent_ban == True:
-            client.send(f'Server: {target_username} has been banned.\n\tReason: {reason}'.encode())
+            send_message(client ,f'Server: {target_username} has been banned.\n\tReason: {reason}')
         else:
-            broadcast(f"{username}: {prefix}ban {target_username} {reason}".encode())
-            broadcast(f"Server: '{target_username}' has been banned.\n\tReason: {reason}".encode())
+            broadcast(f"{username}: {prefix}ban {target_username} {reason}")
+            broadcast(f"Server: '{target_username}' has been banned.\n\tReason: {reason}")
+        user_leave(target_client, error_message['Close'])
 
     #unbans users
     @command('unban', 3)
@@ -125,7 +127,7 @@ def main():
             if (arguments[1]).lower() == 'silent':
                 silent_unban = True
 
-        with open('Tcp Server\\users.json', 'r') as f:
+        with open(file_path, 'r') as f:
             data = json.load(f)
 
             user_role_id = data[username]['role_id']
@@ -133,26 +135,26 @@ def main():
             if target_username in data:
                 target_role_id = data[arguments[0]]['role_id'] 
             else:
-                client.send(f'Server: No user named {target_username} was found!'.encode())
+                send_message(client ,f'Server: No user named {target_username} was found!')
                 return
 
         if len(arguments) > 2:
-            client.send(f'Server: This command only takes 2 argument. {len(arguments)} Given!'.encode())
+            send_message(client ,f'Server: This command only takes 2 argument. {len(arguments)} Given!')
             return
         if user_role_id <= target_role_id: #permission check
-            client.send(f'Server: Server: You cannot unban this member!'.encode())
+            send_message(client ,f'Server: Server: You cannot unban this member!')
             return
         if target_username not in banned_users: #checks to see if they are not in users
-            client.send('Server: User is not currently banned!'.encode())
+            send_message(client ,'Server: User is not currently banned!')
             return
 
         banned_users.remove(target_username)
         
         if silent_unban == True:
-            client.send(f'Server: {target_username} has been unbanned.'.encode())
+            send_message(client ,f'Server: {target_username} has been unbanned.')
         else:
-            broadcast(f'{username}: {prefix}unban {target_username}'.encode())
-            broadcast(f'Server: {target_username} has been unbanned by: {username}.'.encode())
+            broadcast(f'{username}: {prefix}unban {target_username}')
+            broadcast(f'Server: {target_username} has been unbanned by: {username}.')
 
         print(f'Server: Command: {prefix}unban {target_username} silent={silent_unban} (Username: {username})')
 
@@ -169,7 +171,7 @@ def main():
                 silent_kick = True
                 reason = " ".join(arguments[2:])
 
-        with open('Tcp Server\\users.json', 'r') as f:
+        with open(file_path, 'r') as f:
             data = json.load(f)
             
             user_role_id = data[username]['role_id']
@@ -177,31 +179,32 @@ def main():
             if target_username in data:
                 target_role_id = data[arguments[0]]['role_id'] 
             else:
-                client.send(f'Server: No user named {target_username} was found!'.encode())
+                send_message(client ,f'Server: No user named {target_username} was found!')
                 return
             
         target_role_id = data[target_username]['role_id']
 
         if user_role_id <= target_role_id:
-            client.send(f'Server: You cannot kick this member!'.encode())
+            send_message(client ,f'Server: You cannot kick this member!')
             return
         if target_username in banned_users:
-            client.send(f'Server: This member is not currently in the server'.encode())
+            send_message(client ,f'Server: This member is not currently in the server')
             return
         if target_username not in users:
-            client.send(f'Server: This member is not currently in the server'.encode())
+            send_message(client ,f'Server: This member is not currently in the server')
             return
 
         target_client = clients[users.index(target_username)]
-        target_client.send(f'Server: You have been kicked.\n\tReason: {reason}'.encode())
+        send_message(target_client ,f'Server: You have been kicked.\n\tReason: {reason}')
         print(f'Command: {prefix}kick {target_username} Silent={silent_kick} Reason:{reason} (Username: {username})')
         user_leave(target_client, error_message['Close'])
 
         if silent_kick == True:
-            client.send(f'{target_username} has been kicked\n\tReason: {reason}.'.encode())
+            send_message(client ,f'{target_username} has been kicked\n\tReason: {reason}.')
         else:
-            broadcast(f'{username}: {prefix}kick {target_username} {reason}'.encode())
-            broadcast(f'Server: {target_username} has been kicked.\n\tReason: {reason}'.encode())
+            print(reason)
+            broadcast(f'{username}: {prefix}kick {target_username} {str(reason)}')
+            broadcast(f'Server: {target_username} has been kicked.\n\tReason: {str(reason)}')
 
     @command('help', 2)
     def help_command(username, arguments=None):
@@ -212,39 +215,39 @@ def main():
 
         if arguments != None:
             if len(arguments) != 1:
-                client.send(f'Server: This command only takes 1 argument. {len(arguments)} Given!'.encode())
+                send_message(client ,f'Server: This command only takes 1 argument. {len(arguments)} Given!')
                 return
             command_name = arguments[0]
             try:
                 role_id = commands[command_name][1]
                 detials = (f'-{command_name}-\n{descriptions[command_name]["description"]}\n\n-Usage-\n{descriptions[command_name]["usage"]}\n\n-Examples-\n{descriptions[command_name]["examples"]}\n\n-Permissions-\nPermission Level : {role_id}+')
-                client.send(f'Server:\n{detials}'.encode())
+                send_message(client ,f'Server:\n{detials}')
                 print(f'Server: Command: {prefix}help {command_name} (Username: {username})')
                 return
             except KeyError:
-                client.send(f'Server: Could not find any command named: {command_name}'.encode())
+                send_message(client ,f'Server: Could not find any command named: {command_name}')
                 return
         index = users.index(username)
         client = clients[index]
         help_text = (f'Server: [bold white]Commands[/bold white][green]\n{prefix}help \[command]\n{prefix}users\n{prefix}connections\n{prefix}banned-users\n{prefix}kick\n{prefix}ban\n{prefix}unban\n')
-        client.send(help_text.encode())
+        send_message(client ,help_text)
 
         print(f'Server: Command: {prefix}help (Username: {username})')
     
     #message for missing permission
     def missing_permissions(client, message):
-        client.send(f'Server: Missing permissions for the command named: {message.lower()}'.encode())
+        send_message(client ,f'Server: Missing permissions for the command named: {message.lower()}')
     
     #broadcasts a message to all clients. Can send to all axcept certain clients.
     def broadcast(message, dont_send_to=None):
         for client in clients:
             if dont_send_to != None:
                 if dont_send_to != client:
-                    client.send(message)
+                    send_message(client ,message)
                 else:
                     pass
             else:
-                client.send(message)
+                send_message(client ,message)
 
     #command proccesing, checks for permissions and command validity
     def proccess_command(client, message, username):
@@ -252,10 +255,10 @@ def main():
         try:
             command = commands[command_name][0]
         except KeyError:
-            client.send(f'Server: Invalid Command! no command named: {command_name.lower()}'.encode())
+            send_message(client ,f'Server: Invalid Command! no command named: {command_name.lower()}')
             return
 
-        with open('Tcp Server\\users.json', 'r') as f:
+        with open(file_path, 'r') as f:
             data = json.load(f)
 
             role_id = data[username]['role_id']
@@ -265,31 +268,43 @@ def main():
                 try:
                     command(username, arguments=arguments) #checks if message is valid for args
                 except TypeError:
-                    client.send(f'Server: Command: {command_name} : Requires no arguments!'.encode())
+                    send_message(client ,f'Server: Command: {command_name} : Requires no arguments!')
                     return
             else:
                 try:
                     command(username)
                 except TypeError:
-                    client.send(f'Server: Command: {command_name} : Requires 1 or more arguments!'.encode())
+                    send_message(client ,f'Server: Command: {command_name} : Requires 1 or more arguments!')
                     return
         else:
             missing_permissions(client ,command_name)      
+
+    def recv_message(client):
+        message = client.recv(1024).decode()
+        return message[3:]
+    def send_message(client, message):
+        length = str(len(message)).zfill(3)
+        client.send(f'{length}{message}'.encode())
+        return
+
+
 
     #main client listener for messages and commands.
     def handle_client(client):
         while True:
             try:
-                message = client.recv(1024).decode()
+                message = recv_message(client)
                 username = users[clients.index(client)]
-
-                if len(message) > 200:
-                    client.send('Server: Message size exceeds the 200 character limit!'.encode())
+                
+                if len(message) == 0:
+                    pass
+                elif len(message) > 200:
+                    send_message(client ,'Server: Message size exceeds the 200 character limit!')
                     continue
-                if message[:len(prefix)] == prefix: #command prefix
+                elif message[:len(prefix)] == prefix: #command prefix
                     proccess_command(client, message[len(prefix):].lower(), username)
                 else:
-                    broadcast(f'{username}: {message}'.encode())
+                    broadcast(f'{username}: {message}')
             except ConnectionAbortedError:
                 break #Server Closed Connetion
             except ConnectionResetError:
@@ -308,11 +323,11 @@ def main():
             username = 'N/A'
 
         if broadcast_leave == True:
-            broadcast(f'Server: {username} has disconnected...'.encode())
+            broadcast(f'Server: {username} has disconnected...')
         print(f'Server: Client:({username} {address} has disconnected (Reason: {leave_reason})')
     
     def failed_login(client):
-        client.send('Username or Password is incorrect!'.encode())
+        send_message(client ,'Username or Password is incorrect!')
         user_leave(client, 'User failed login', broadcast_leave=False)
 
     #accepts clients, and checks login crediantials.
@@ -320,29 +335,29 @@ def main():
         while True:
             client, address = server.accept()
             print(f'{str(address)} has connected...')
-            client.send('LOGIN'.encode()) #Sends message to start the login proccess. May change to be the first 2 inputs rather than sending a message.
+            send_message(client ,'LOGIN') #Sends message to start the login proccess. May change to be the first 2 inputs rather than sending a message.
 
             try:
-                username = client.recv(1024).decode().lower()[1:]
-                password = client.recv(1024).decode()[1:]
+                username = recv_message(client)[1:]
+                password = recv_message(client)[1:]
             except ConnectionResetError: #User left during login
                 user_leave(client, 'Left during login')
                 continue
 
             if username in banned_users:
-                client.send('You are banned!'.encode())
+                send_message(client ,'You are banned!')
                 print('User is banned. Connection ended...')
                 client.close()
                 continue
             if username in users:
-                client.send('User already connected!'.encode())
-                user_leave(client, error_message['Conn'], broadcast_leave=False)
+                send_message(client ,'User already connected!')
+                user_leave(client, error_message['Connection'], broadcast_leave=False)
                 continue
             else:
                 pass
 
             #No hashing or anything of the sort is being added until later.
-            with open('Tcp Server\\users.json', 'r') as f:
+            with open(file_path, 'r') as f:
                 data = json.load(f)
 
                 if username in data:
@@ -355,7 +370,7 @@ def main():
                 user_password = user[1]
 
                 if user_password == password:
-                    client.send('Logged In!'.encode())
+                    send_message(client ,'Logged In!')
                                         
                     clients.append(client)
                     users.append(username.lower())
@@ -363,8 +378,8 @@ def main():
                     failed_login(client)
                     continue
 
-            broadcast(f'Server: {username} has connected...'.encode(), dont_send_to=client) #sends a message to everyone but that client
-            client.send(f'Server: You are connected to : {address}'.encode())
+            broadcast(f'Server: {username} has connected...', dont_send_to=client) #sends a message to everyone but that client
+            send_message(client ,f'Server: You are connected to : {address}')
 
             t1 = threading.Thread(target=handle_client, args=(client,))
             t1.start()
