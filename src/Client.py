b68a25bfad880c1b3d474b3d94bd9e58a195fd47
Redriver <80642468+TheRealRedRiver559@users.noreply.github.com>
2022-03-20 19:12:59 -0700

    Update Client.py
    Changed the structure to a class for a better login system and a 're-connect' feature I added.

--- src/Client.py
+++ src/Client.py
@@ -4,39 +4,58 @@ import datetime
 from rich import print
 import json
 
-username = input('Username: ')
+class client():
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.username = None
+        self.connected = False
+    
+    def connect(self, client):
+        while self.connected == False:
+            if client.connect_ex((self.host, self.port)) != 0:
+                pass
+            else:
+                print('Connected!')
+                self.connected = True
+                self.login()
 
-def get_time():
-    time = datetime.datetime.now()
-    setting = 'AM'
-    hours = int(time.strftime("%H"))
-    if hours > 12:
-        setting = 'PM'
-        hours -= 12
-    return time.strftime(f"%m/%d/%Y {hours}:%M {setting}")
+    def login(self):
+        self.username = input('Username: ')
+    
+    @staticmethod
+    def get_time():
+        time = datetime.datetime.now()
+        setting = 'AM'
+        hours = int(time.strftime("%H"))
+        if hours > 12:
+            setting = 'PM'
+            hours -= 12
+        return (time.strftime(f"%m/%d/%Y {hours}:%M {setting}"))
+    
+    def send_message(self, client):
+        while True:
+            message = input()
+            data = json.dumps({'username':self.username, 'message':message})
+            client.send(data.encode())
 
-def send_message(client):
-    while True:
-        message = input()
-        data = json.dumps({'username':username, 'message':message})
-        client.send(data.encode())
+    def recv_message(self, client):
+        while True:
+            message_data = json.loads(client.recv(1024).decode())
+            message = f"{message_data['username']}: {message_data['message']}"
 
-def recv_message(client):
-    while True:
-        message_data = json.loads(client.recv(1024).decode())
-        message = f"{message_data['username']}: {message_data['message']}"
+            if message_data['username'] == self.username: #Blue for your username
+                print(f"[blue]{message[:len(self.username)]} [white]{message[len(self.username):]} [grey37]{self.get_time()}")
+            else:
+                print(f"[red]{message[:len(message_data['username'])]} [white]{message[len(message_data['username']):]} [grey37]{self.get_time()}")
+    
+    def run_client(self):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect(client)
+        recv_thread = threading.Thread(target=self.recv_message, args=(client,))
+        recv_thread.start()
+        message_thread = threading.Thread(target=self.send_message, args=(client,))
+        message_thread.start()
 
-        if message_data['username'] == username: #Blue for your username
-            print(f"[blue]{message[:len(username)]} [white]{message[len(username):]} [grey37]{get_time()}")
-        else:
-            print(f"[red]{message[:len(message_data['username'])]} [white]{message[len(message_data['username']):]} [grey37]{get_time()}")
-
-def run_client():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect_ex(('127.0.0.1', 9090))
-    recv_thread = threading.Thread(target=recv_message, args=(client,))
-    recv_thread.start()
-    message_thread = threading.Thread(target=send_message, args=(client,))
-    message_thread.start()
-
-run_client()
+Client = client('127.0.0.1', 9090)
+Client.run_client()
