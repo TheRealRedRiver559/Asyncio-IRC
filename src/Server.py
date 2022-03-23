f007498b4632c781bc27fe52ed2be02b9aa45d49
Redriver <80642468+TheRealRedRiver559@users.noreply.github.com>
2022-03-22 21:48:44 -0700

    Update Server.py

--- src/Server.py
+++ src/Server.py
@@ -1,18 +1,27 @@
 import asyncio
+import json
 import socket
 
 clients = []
+
 async def broadcast(message):
     for client in clients:
         loop = asyncio.get_event_loop()
-        await loop.sock_sendall(client, message.encode())
+        await loop.sock_sendall(client, message)
 
 async def handle_client(client):
     loop = asyncio.get_event_loop()
+    
     while True:
         try:
-            message = (await loop.sock_recv(client, 1024)).decode()
-            await broadcast(message)
+            message = json.loads((await loop.sock_recv(client, 1024)).decode())
+            
+            if message['length'] > 200:
+                message = json.dumps({'username':'Server', 'message':'Message too large'}).encode()
+                await loop.sock_sendall(client, message)
+            else:
+                message = json.dumps({'username':message['username'], 'message':message['message']}).encode()
+                await broadcast(message)
         except ConnectionResetError:
             client.close()
             clients.remove(client)
@@ -26,8 +35,12 @@ async def run_server():
 
     loop = asyncio.get_event_loop()
     while True:
-        client, adr = await loop.sock_accept(server)
+        client, addr = await loop.sock_accept(server)
         clients.append(client)
+        login_data = json.loads((await loop.sock_recv(client, 1024)).decode())
+        print(login_data)
+        print(addr)
+        
         loop.create_task(handle_client(client))
 
 asyncio.run(run_server())
