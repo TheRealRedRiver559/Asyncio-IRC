aad9ccfdf2405160d2a53a80790a4a7fe40e16d0
RedRiver <80642468+TheRealRedRiver559@users.noreply.github.com>
2024-01-08 16:30:47 -0700

    Create settings.py

---
+++ src/misc/settings.py
@@ -0,0 +1,9 @@
+
+CHAT_HISTORY = False
+HOST, PORT = ("localhost", 9090)
+MESSAGE_SIZE = 200
+USERNAME_LEN = 10
+ESCAPE_CODE = "\\"
+CERT_PATH, KEY_PATH = "./certs/cert.pem", "./certs/private.key"  # self signed cert, and
+SSL_CONNECTIONS = False
+PING_INTERVAL = 5 # 5 second ping interval
